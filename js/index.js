e050d633ea019581a3d325fa5e76d742d97a4fca
SlawaSlawa <slawapocht4@yandex.ru>
2023-03-05 21:25:03 +0400

    added css & slider for section reason

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
-console.log('script');
+const swiper = new Swiper('.reason__slider', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 133,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+});
 console.log('script2');
 console.log('script3!');
