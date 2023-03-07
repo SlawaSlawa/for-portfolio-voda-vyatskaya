fcd045a382ee653378c90db09f48c01ef18bf99c
SlawaSlawa <slawapocht4@yandex.ru>
2023-03-07 13:07:57 +0400

    added @media for section reason

--- js/index.js
+++ js/index.js
@@ -1,10 +1,31 @@
 const swiper = new Swiper('.reason__slider', {
-    loop: true,
     slidesPerView: 3,
     spaceBetween: 133,
+    initialSlide: 1,
+    loop: false,
     pagination: {
         el: '.swiper-pagination',
+        clickable: true,
     },
+    breakpoints: {
+        280: {
+            spaceBetween: 0,
+            centeredSlides: true,
+            initialSlide: 1,
+        },
+        992: {
+                spaceBetween: 133,
+                centeredSlides: false,
+                initialSlide: 1,
+            }
+        },
+    on: {
+        resize(evt) {
+            if (this.width < 992) {
+                this.activeIndex = 1
+            }
+        }
+    }
 
 });
 console.log('script2');
