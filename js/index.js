083785b2df597f001d543f0845c19c3dfc0b9fea
SlawaSlawa <slawapocht4@yandex.ru>
2023-03-11 19:23:45 +0400

    added @media for section catalog

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.reason__slider', {
+const swiperReason = new Swiper('.reason__slider', {
     slidesPerView: 3,
     spaceBetween: 133,
     initialSlide: 1,
@@ -28,5 +28,88 @@ const swiper = new Swiper('.reason__slider', {
     }
 
 });
-console.log('script2');
-console.log('script3!');
+const swiperCatalog = new Swiper('.catalog-slider', {
+    slidesPerView: 4,
+    spaceBetween: 29,
+    initialSlide: 0,
+    centeredSlides: false,
+    
+    breakpoints: {
+        280: {
+            spaceBetween: 18,
+            slidesPerView: 1.3,
+            centeredSlides: true,
+            initialSlide: 1,
+        },
+        300: {
+            spaceBetween: 18,
+            slidesPerView: 1.4,
+            centeredSlides: true,
+            initialSlide: 1,
+        },
+        320: {
+            spaceBetween: 18,
+            slidesPerView: 1.5,
+            centeredSlides: true,
+            initialSlide: 1,
+        },
+        390: {
+            spaceBetween: 22,
+            slidesPerView: 1.7,
+            centeredSlides: true,
+            initialSlide: 1,
+        },
+        600: {
+            spaceBetween: 22,
+            slidesPerView: 2.3,
+            centeredSlides: true,
+            initialSlide: 1,
+        },
+        800: {
+            spaceBetween: 22,
+            slidesPerView: 3.3,
+            centeredSlides: true,
+            initialSlide: 1,
+        },
+        976: {
+                spaceBetween: 29,
+                slidesPerView: 4,
+                centeredSlides: false,
+                initialSlide: 0,
+            }
+        },
+        on: {
+            resize(evt) {
+                if (this.width < 976) {
+                    this.activeIndex = 1
+                }
+            }
+        }
+});
+const catalogNavigationSlider = new Swiper('.catalog__navigation-slider', {
+    slidesPerView: 4,
+    spaceBetween: 24,
+    initialSlide: 0,
+    
+    breakpoints: {
+        280: {
+            spaceBetween: 22,
+            slidesPerView: 2,
+            centeredSlides: true,
+            centeredSlidesBounds: true,
+        },
+        520: {
+            spaceBetween: 22,
+            slidesPerView: 3,
+            centeredSlides: true,
+            initialSlide: 0,
+            centeredSlidesBounds: true,
+        },
+        700: {
+            spaceBetween: 22,
+            slidesPerView: 4,
+            centeredSlides: false,
+            centeredSlidesBounds: false,
+        },
+        },
+});
